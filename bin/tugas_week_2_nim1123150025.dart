d9e6537c3df845b77cec73946b304ae3d9ec183a
agrahafiz13 <agrahafiz2@gmail.com>
2025-09-26 22:53:13 +0700

    first commit

---
+++ bin/tugas_week_2_nim1123150025.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week_2_nim1123150025/tugas_week_2_nim1123150025.dart' as tugas_week_2_nim1123150025;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week_2_nim1123150025.calculate()}!');
+}
---
+++ test/tugas_week_2_nim1123150025_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_week_2_nim1123150025/tugas_week_2_nim1123150025.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
